09940e03ff315165f22f5a027674ea937c738106
Ron Klinker <r.klinker@gmail.com>
2022-12-17 15:11:31 +0100

    ready for first deploy

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='home')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html', title='about')
+
+if __name__ == '__main__':
+    app.run()
