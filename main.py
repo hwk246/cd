59e1b59a1754253f8cf82bfa4f416deb54cebb4f
Ron Klinker <r.klinker@gmail.com>
2022-12-21 22:35:31 +0100

    final update

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    
+def index(): 
     return render_template('index.html', title='home')
 
 @app.route('/about/')
 def about():
     return render_template('about.html', title='about')
 
-@app.route('/handy/')
-def handy():
-    return render_template('handy.html', title='handy')
+@app.route('/xmas/')
+def xmas():
+    return render_template('xmas.html', title='xmas')
 
 if __name__ == '__main__':
     app.run()
